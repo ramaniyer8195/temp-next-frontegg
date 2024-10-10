7952bda7db30d2d2e2c29f2983adf18398d656fa
Raman-Iyer <71245553+Raman-Iyer@users.noreply.github.com>
2024-10-10 16:05:39 +0530

    config fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,9 @@ import { withFronteggApp } from "@frontegg/nextjs/pages";
 import React from "react";
 import "../../styles/globals.css";
 
-// @ts-expect-error return type will be defined later
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }): React.JSX.Element => {
   return <Component {...pageProps} />;
-}
+};
 
 export default withFronteggApp(MyApp, {
   hostedLoginBox: true,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,23 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
 
-// @ts-expect-error return type will be defined later
-const Home = () => {
+const Home = (): React.JSX.Element => {
   const { user } = useAuth();
   const router = useRouter();
 
-  const handleLogout = () => {
+  const handleLogout = (): void => {
     router.replace("/account/logout");
   };
 
   return (
     <div>
       <p>Welcome {user?.name}</p>
-      <Image src={user?.profilePictureUrl || ""} width="96px" height="96px" />
+      <Image
+        alt="Profile picture"
+        height="96px"
+        src={user?.profilePictureUrl || ""}
+        width="96px"
+      />
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
